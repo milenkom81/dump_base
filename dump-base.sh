64168a3f6c923c41020a1fc34d358892685a6113
milenkom81 <milenkom81@gmail.com>
2018-06-19 15:17:17 +0200

    Add files via upload

---
+++ dump-base.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+# Skriptica za backup fajlova i baza sa vps-a
+#Author: Milenko Mitrović
+#
+#System setup
+NOW=`date +%Y-%m-%d`
+KEEPDAYS=30
+
+#Local folder for backup
+loc_loc=~/backup
+
+#Server list
+cat servers.txt | cut -f1 | while read servername
+
+do 
+
+
+#Entry parameters for VPS's from servers.txt
+
+dbname=`cat servers.txt |grep $servername | awk '{printf $2}'`
+dbuser=`cat servers.txt |grep $servername | awk '{printf $3}'`
+dbpass=`cat servers.txt |grep $servername | awk '{printf $4}'`
+rem_loc=`cat servers.txt |grep $servername | awk '{printf $5}'`
+ruser=`cat servers.txt |grep $servername | awk '{printf $6}'`
+
+#test command not used for anything just checking readout from servers.txt
+echo $servername
+# If there is no directory by name of the server create it
+if [ ! -d "$loc_loc/$servername" ]; then
+mkdir -p "$loc_loc"/"$servername"
+fi
+#File where to dump base - first check is there database folder
+if  [ ! -d "$loc_loc/$servername/database" ]; then
+mkdir -p "$loc_loc"/"$servername/database"
+fi
+
+FILE="$loc_loc"/"$servername"/mysql$servername-$NOW.gz
+#Dump MySql database to file
+ssh $ruser@$servername "mysqldump -q -u $dbuser -h $servername -p $dbpass $dbname | gzip -9" > $FILE
+#Copy files from server
+rsync -zcvf $ruser@$servername:$rem_loc/* $loc_loc/$servername/$servername_$NOW.tar.gz
+
+done
+# Version history
+#------------------
+# 1.0  setup basic read of parameters and dump mysql baze
+
+
+
+
+
+
